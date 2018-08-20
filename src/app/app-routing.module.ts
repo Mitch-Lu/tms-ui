13baf6b132a14a0b61fb8bf1608c091a13f22064
Mitchell-Lu <Mitchell.Lu@xpo.com>
2018-08-20 13:51:41 -0700

    Added routing-module

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+
+const routes: Routes = [
+  
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  declarations: [RouterModule]
+})
+export class AppRoutingModule { }
